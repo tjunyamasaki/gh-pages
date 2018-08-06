79f7063ef1f78fcae11fd4a1c1dfbc6cf68fca85
Thomas Jun Yamasaki <tjun.yamasaki@gmail.com>
2018-08-06 12:17:14 -0300

    test

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+// const axios = require('axios');
+import qs from 'qs';
 
-const Home = () => (
-  <div>
-    <h2>Home Page</h2>
-  </div>
-);
+class Home extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      response: ""
+    }
+  }
+  componentWillMount(){
+
+    // const options = {
+    //   method: 'GET',
+    //   headers: { 'content-type': 'application/x-www-form-urlencoded'},
+    //   url: 'http://18.216.134.103:4001/',
+    // };
+    // axios(options)
+    // .then(res => {
+    //   console.log(res.data);
+    //   this.setState({response: res.data.response})
+    // })
+    // .catch(error => {
+    //   console.log(error.response)
+    // });
+
+  }
+  render(){
+    return(
+      <div>
+        <h2>Home Page</h2>
+        {this.state.response}
+      </div>
+
+    )
+  }
+}
 
 export default Home;
